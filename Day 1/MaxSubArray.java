103c8963867546fd8848ef51bbd58bf796a97567
hemanth <hemanthbabu6041@gmail.com>
2021-12-08 12:22:00 +0530

    solved Maximum SubArray

---
+++ Day 1/MaxSubArray.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/maximum-subarray/
+class maxSubArray {
+    public int maxSubArray(int[] nums) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for (int num : nums) {
+            currSum += num;
+            if (currSum > maxSum) {
+                maxSum = currSum;
+            }
+            if (currSum < 0) {
+                currSum = 0;
+            }
+        }
+        return maxSum;
+    }
+}
